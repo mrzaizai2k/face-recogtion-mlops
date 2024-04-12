71fd2aa3eaf4f03353b8025c980bbc9c51a8e975
Mai Chí Bảo <chibao24.12.1999@gmail.com>
2024-04-12 15:13:34 +0700

    Update producer.py

--- src/kafka/producer.py
+++ src/kafka/producer.py
@@ -1,121 +1,162 @@
+import json
+import re
 import sys
 sys.path.append("")
-import time 
-import json 
-import random 
-from datetime import datetime
-from kafka import KafkaProducer
-
-# Messages will be serialized as JSON 
-# def serializer(message):
-#     return json.dumps(message).encode('utf-8')
-
-# # Kafka Producer
-# producer = KafkaProducer(
-#     bootstrap_servers=['localhost:9092'],
-#     value_serializer=serializer
-# )
-
-# if __name__ == '__main__':
-#     # Infinite loop - runs until you kill the program
-#     while True:
-#         # Generate a message
-#         dummy_message = generate_message()
-        
-#         # Send it to our 'messages' topic
-#         print(f'Producing message @ {datetime.now()} | Message = {str(dummy_message)}')
-#         producer.send('messages', dummy_message)
-        
-#         # Sleep for a random number of seconds
-#         time_to_sleep = random.randint(1, 11)
-#         time.sleep(time_to_sleep)
-
-
-from glob import glob
-import concurrent.futures
-from src.kafka.utils import *
-import os
-import cv2
 import time
+from multiprocessing import Process
+
+import cv2
+import imutils
+import numpy as np
+from imutils.video import VideoStream
+from kafka import KafkaProducer
+from src.kafka.utils import *
 
 
-def resize_image(image, new_height=480):
-    """
-    Resize an image while maintaining the aspect ratio.
+class StreamVideo(Process):
 
-    Args:
-    - image: numpy array representing the image
-    - new_height: desired height of the resized image
+    def __init__(self, video_path,
+                 topic,
+                 use_cv2=False,
+                 pub_obj_key="original", #should always be "original" in our case
+                 group=None,
+                 target=None,
+                 camera_name=None,
+                 verbose=False,):
+        """Video Streaming Producer Process Class. Publishes frames from a video source to a topic.
 
-    Returns:
-    - resized_image: resized image numpy array
-    """
-    # Calculate the aspect ratio
-    height, width = image.shape[:2]
-    aspect_ratio = width / height
-    
-    # Calculate the new width based on the aspect ratio and the desired height
-    new_width = int(new_height * aspect_ratio)
-    
-    # Resize the image
-    resized_image = cv2.resize(image, (new_width, new_height))
-    
-    return resized_image
+        :param video_path: video path or url
+        :param topic: kafka topic to publish stamped encoded frames.
+        :param topic_partitions: number of partitions this topic has, for distributing messages among partitions
+        :param use_cv2: send every frame, using cv2 library, else will use imutils to speedup training. use_cv2 = True is for camera, else is for videos
+        :param pub_obj_key: associate tag with every frame encoded, can be used later to separate raw frames
+        :param group: group should always be None; it exists solely for compatibility with threading.
+        :param target: Process Target
+        :param name: Process name
+        :param verbose: print logs on stdout
+        :param rr_distribute: use round robin partitioner, should be set same as consumers.
 
-class ProducerThread:
-    def __init__(self):
-        # self.producer = Producer(config)
-        self.producer = KafkaProducer(
-            bootstrap_servers=['localhost:9092'],
-            # value_serializer=serializer
-        )
+        """
 
-    def publishFrame(self, video_path):
-        video = cv2.VideoCapture(video_path)
-        video_name = os.path.basename(video_path).split(".")[0]
-        frame_no = 1
-        while video.isOpened():
-            _, frame = video.read()
-            # pushing every 3rd frame
-            if frame_no % 3 == 0:
-                print(frame_no)
-                frame = resize_image(frame)
-                frame_bytes = serializeImg(frame)
-                timestamp_ms = int(time.time() * 1000)
-                self.producer.send(
-                    topic="message", 
-                    value=frame_bytes, 
-                    headers=[('timestamp', str(timestamp_ms).encode()), 
-                             ("video_name", str.encode(video_name)),
-                             ]
-                    # timestamp_ms=frame_no,
-                    # headers={
-                    #     "video_name": str.encode(video_name)
-                    # }
-                    
-                )
-                # self.producer.poll(0)
-            time.sleep(1)
-            frame_no += 1
+        super().__init__(group=group, target=target, name=camera_name)
+
+        # URL for streaming video
+        self.video_path = video_path
+        # TOPIC TO PUBLISH
+        self.frame_topic = topic
+
+        self.use_cv2 = use_cv2
+        self.object_key = pub_obj_key
+        self.verbose = verbose
+
+    def run(self):
+        """Publish video frames as json objects, timestamped, marked with camera number.
+
+        Source:
+            self.video_path: URL for streaming video
+            self.kwargs["use_cv2"]: use raw cv2 streaming, set to false to use smart fast streaming --> not every frame is sent.
+        Publishes:
+            A dict {"frame": string(base64encodedarray), "dtype": obj.dtype.str, "shape": obj.shape,
+                    "timestamp": time.time(), "camera": camera, "frame_num": frame_num}
+        """
+
+
+        # Producer object, set desired partitioner
+        frame_producer = KafkaProducer(bootstrap_servers=["localhost:9092"],
+                                       key_serializer=lambda key: str(key).encode(),
+                                       value_serializer=lambda value: json.dumps(value).encode(),
+                                       )
+
+        print(f"[CAM {self.name}] URL: {self.video_path}, SET PARTITIONS FOR FRAME TOPIC: {self.frame_topic}")
+        # Use either option
+        video = cv2.VideoCapture(self.video_path) if self.use_cv2 else VideoStream(self.video_path).start()
+
+        # Track frame number
+        frame_num = 0
+        start_time = time.time()
+        print(f"[CAM {self.name}] START TIME {start_time}: ")
+
+        # Read URL, Transform, Publish
+        while True:
+
+            # using raw cv2, frame by frame
+            if self.use_cv2:
+                success, image = video.read()
+                # check if the file has read
+                if not success:
+                    if self.verbose:
+                        print(f"[CAM {self.name}] URL: {self.video_path}, END FRAME: {frame_num}")
+                    break
+
+            # using smart, only unique frames, skips frames, faster fps
+            else:
+                image = video.read()
+                # check if the file has read
+                if image is None:
+                    if self.verbose:
+                        print(f"[CAM {self.name}] URL: {self.video_path}, END FRAME: {frame_num}")
+                    break
             
-        video.release()
-        return
-        
-    def start(self, vid_paths):
-        # runs until the processes in all the threads are finished
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(self.publishFrame, vid_path) for vid_path in vid_paths]
-            concurrent.futures.wait(futures)  # Wait for all threads to finish
 
-        self.producer.flush() # push all the remaining messages in the queue
-        print("Finished...")
+            # Attach metadata to frame, transform into JSON
+            message = self.transform(frame=image,
+                                     frame_num=frame_num,
+                                     object_key=self.object_key,
+                                     camera_name=self.name,
+                                     verbose=self.verbose)
 
 
+            # Publish to specific partition
+            frame_producer.send(self.frame_topic, key=f"{self.name}_{frame_num}", value=message)
 
-if __name__ == "__main__":
-    video_dir = "test_data/"
-    video_paths = glob(video_dir + "*.mp4") # change extension here accordingly
-    print(video_paths)
-    producer_thread = ProducerThread()
-    producer_thread.start(video_paths)
+            # if frame_num % 1000 == 0:
+            frame_producer.flush()
+
+            frame_num += 1
+
+        # clear the capture
+        if self.use_cv2:
+            video.release()
+        else:
+            video.stop()
+
+        if self.verbose:
+            print(f"[CAM {self.name}] FINISHED. STREAM TIME {time.time() - start_time}: ")
+
+        return True if frame_num > 0 else False
+
+    @staticmethod
+    def transform(frame, frame_num, object_key="original", camera_name="192.168.1.2", verbose=False):
+        """Serialize frame, create json message with serialized frame, camera number and timestamp.
+
+        :param frame: numpy.ndarray, raw frame
+        :param frame_num: frame number in the particular video/camera
+        :param object_key: identifier for these objects
+        :param camera: Camera Number the frame is from
+        :param verbose: print out logs
+        :return: A dict {"frame": string(base64encodedarray), "dtype": obj.dtype.str, "shape": obj.shape,
+                    "timestamp": time.time(), "camera": camera, "frame_num": frame_num}
+        """
+        frame = imutils.resize(frame, width=480)
+
+        if verbose:
+            # print raw frame size
+            print("\nRAW ARRAY SIZE: ", sys.getsizeof(frame))
+
+        # serialize frame
+        frame_dict = np_to_json(frame.astype(np.uint8), prefix_name=object_key)
+        # Metadata for frame
+        message = {"timestamp": time.time(), "camera": camera_name, "frame_num": frame_num}
+        # add frame and metadata related to frame
+        message.update(frame_dict)
+
+        if verbose:
+            # print message size
+            print("\nMESSAGE SIZE: ", sys.getsizeof(message))
+
+        return message
     
+if __name__=="__main__":
+    # test/kafka_videos/video-1.mp4
+    producer = StreamVideo(video_path="test/kafka_videos/video-1.mp4", topic="message", camera_name="192.168.2.3")
+    producer.run()
